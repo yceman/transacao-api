5bc969efbb820ee8d505602fba0b202c4a3c89a2
Marcos Morais <marcosmorais.v10@gmail.com>
2025-05-06 21:32:54 -0300

    First Commit

---
+++ src/main/java/com/bank/transacao_api/TransacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.bank.transacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoApiApplication.class, args);
+	}
+
+}
